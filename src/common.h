df971a2b837398954fd317258506c04afc4a4e66
ArqAlice <y.tomi0131@gmail.com>
2025-12-29 02:06:25 +0900

    THD補正を調整。

--- src/common.h
+++ src/common.h
@@ -68,11 +68,11 @@
 #define I2C_ESS_DAC_ADDR (ADDR0_ES9038Q2M)
 #define ENABLE_ES9038Q2M_DEPOP (true)
 #define ENABLE_ESS_DAC_VOLUME (true)
-#define ENABLE_ESS_DAC_THD_COMPEN (false)
-#define ESS_THD_COMPEN_C2 (0) // 16bit signed int
-#define ESS_THD_COMPEN_C3 (0) // 16bit signed int
-#define ESS_DPLL_BANDWIDTH (0xA0) // 0~255, 0 is DPLL off
-#define ESS_DPLL_LOCKSPEED (16)   // 0~16
+#define ENABLE_ESS_DAC_THD_COMPEN (true)
+#define ESS_THD_COMPEN_C2 (3) // 16bit signed int
+#define ESS_THD_COMPEN_C3 (-99) // 16bit signed int
+#define ESS_DPLL_BANDWIDTH (0xC0) // 0~255, 0 is DPLL off
+#define ESS_DPLL_LOCKSPEED (2)   // 0~16
 #define TIME_ES9038Q2M_DEPOP_USEC (40000)
 #define DAC_ENABLE_PIN (5)
 
